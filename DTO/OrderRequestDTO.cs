d4bca5a2cfffe1d9af9c9cefde2fc83b300d2b1b
Rakesh ipili <2021MT70592@wilp.bits-pilani.ac.in>
2023-11-06 23:47:09 +0530

    First commit

---
+++ DTO/OrderRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineFoodDeliverySystem.DTO
+{
+    public class OrderRequestDTO
+    {
+    
+        public int TotalAmount { get; set; }
+        public string Order_Status { get; set; }
+        public string Delivery_status { get; set; }
+
+        public int user_id { get; set; }
+    }
+}
